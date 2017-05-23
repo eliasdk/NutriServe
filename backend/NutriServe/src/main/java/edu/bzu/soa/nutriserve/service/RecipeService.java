ffa69183c723a31084ed28f382d3a07492bf1a76
Elias Khalil <eliasdkh@gmail.com>
2017-05-23 09:04:01 +0300

    remove json response from service class

--- backend/NutriServe/src/main/java/edu/bzu/soa/nutriserve/service/RecipeService.java
+++ backend/NutriServe/src/main/java/edu/bzu/soa/nutriserve/service/RecipeService.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 import org.json.JSONException;
-import org.json.JSONObject;
+//import org.json.JSONObject;
 
 import edu.bzu.soa.nutriserve.common.CommonHttp;
 import edu.bzu.soa.nutriserve.common.HttpReturnData;
@@ -13,11 +13,11 @@ import edu.bzu.soa.nutriserve.model.Recipe;
 
 public class RecipeService {
 	
-	List<Recipe> getRecipes () throws ClientProtocolException, IOException, JSONException {
-		 HttpReturnData getRes = CommonHttp.getAPI ("http://localhost:9090/api/v1/employee") ;
-		 System.out.println(getRes.getReturnContent());
-		JSONObject resDetails = new JSONObject("{\"employees\":" + getRes.getReturnContent()+ "}");
- 
+	List<Recipe> getRecipes ()  {
+//		 HttpReturnData getRes = CommonHttp.getAPI ("http://localhost:9090/api/v1/employee") ;
+//		 System.out.println(getRes.getReturnContent());
+//		JSONObject resDetails = new JSONObject("{\"employees\":" + getRes.getReturnContent()+ "}");
+// 
 		 
 		return null;
 		
