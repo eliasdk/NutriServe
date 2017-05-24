3d990583953ac1c400cf62ba7b6b092c75ce01fb
Elias Khalil <eliasdkh@gmail.com>
2017-05-24 22:14:05 +0300

    connect Recipes to Edammam Rest APIs

--- backend/NutriServe/src/main/java/edu/bzu/soa/nutriserve/NutriServeApplication.java
+++ backend/NutriServe/src/main/java/edu/bzu/soa/nutriserve/NutriServeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import edu.bzu.soa.nutriserve.service.RecipeService;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
@@ -18,6 +19,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class NutriServeApplication {
 
 	public static void main(String[] args) {
+		
+		RecipeService recipeService = new RecipeService();
+		try {
+			recipeService.getRecipes("Chicken");
+			recipeService.getRecipes("Meat");
+			recipeService.getRecipes("vegetables");
+			recipeService.getRecipes("carrots");
+			recipeService.getRecipes("beans");
+			recipeService.getRecipes("seeds");
+			recipeService.getRecipes("onions");
+			recipeService.getRecipes("pizza");
+			recipeService.getRecipes("salad");
+			recipeService.getRecipes("tuna");
+			recipeService.getRecipes("tomato");
+			recipeService.getRecipes("broccoli");
+			System.out.println("+++++++++++++++++++++++++");
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		SpringApplication.run(NutriServeApplication.class, args);
 	}
 	
--- backend/NutriServe/src/main/java/edu/bzu/soa/nutriserve/controller/RecipeController.java
+++ backend/NutriServe/src/main/java/edu/bzu/soa/nutriserve/controller/RecipeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.bzu.soa.nutriserve.model.Recipe;
+import edu.bzu.soa.nutriserve.service.RecipeService;
+import edu.bzu.soa.nutriserve.common.RecipesData;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
@@ -22,25 +24,34 @@ public class RecipeController {
 	@ApiOperation(value = "Get All Recipes in the system", response = List.class)
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Recipe> getAllReciepes() {
-		return recipes;
+		RecipeService recipeService = new RecipeService();
+		try {
+			if (RecipesData.recipes.size() < 7) {
+				recipeService.getRecipes("onions");
+				recipeService.getRecipes("pizza");
+				recipeService.getRecipes("salad");
+				recipeService.getRecipes("tuna");
+				recipeService.getRecipes("tomato");
+				recipeService.getRecipes("broccoli");
+
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return RecipesData.recipes;
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/{id}")
 	@ApiOperation(value = "Get Recipe with recipe id = id from the system", response = Recipe.class)
-	public Recipe getRecipeWithId(@PathVariable Long id) {
-		Recipe recipe1 = new Recipe();
+	public Recipe getRecipeWithId(@PathVariable int id) {
 
-		recipe1.setCuisine("westren");
-		recipe1.setCalories(1479);
-		recipe1.setDescription("Checken Salad with carrots");
-		recipe1.setId(236);
-		recipe1.setImageUrl("http://images.media-allrecipes.com/userphotos/560x315/2204020.jpg");
-		recipe1.setName("Carrot Salad");
-		recipe1.setRecipeurl(
-				"http://allrecipes.com/recipe/8854/broccoli-chicken-divan/?internalSource=popular&referringContentType=home%20page&clickId=cardslot%209");
-		recipe1.setPreviousRecipeApi("https://bzu-nutriserve.appspot.com/recipe/1");
-		recipe1.setNextRecipeApi("https://bzu-nutriserve.appspot.com/recipe/3");
-		return recipe1;
+		if (id < RecipesData.recipes.size()  ) {
+			return RecipesData.recipes.get(id);
+		}
+		return RecipesData.recipes.get(0);
+
 	}
 
 }
